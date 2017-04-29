47b00463e97092d69d0b960d1c2ed914e5ffd9cf
xano <denis.kraemer@gmx.net>
2017-04-29 16:41:50 +0200

    art preloaded classes optimization
    Change-Id: I9ea534564791edd28e0fe46620312e311c6cc816

--- cancro.mk
+++ cancro.mk
@@ -267,6 +267,11 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth_le.xml:system/etc/permissions/android.hardware.bluetooth_le.xml \
     frameworks/native/data/etc/android.software.print.xml:system/etc/permissions/android.software.print.xml
 
+# art preloaded classes optimization
+# https://source.android.com/devices/tech/dalvik/configure.html
+PRODUCT_COPY_FILES += \
+    frameworks/base/preloaded-classes:system/etc/preloaded-classes
+
 # Screen density
 PRODUCT_AAPT_CONFIG := normal hdpi xhdpi xxhdpi
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
