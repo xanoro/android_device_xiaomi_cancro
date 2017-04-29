45b75905d7fe741d8490e1f7293e84053df1e55e
xano <denis.kraemer@gmx.net>
2017-04-29 17:07:04 +0200

    adjust device power and performance modes
    Change-Id: I0c79d33240fa0234a0047e46fc11e58fc2c7d730

--- configs/changepowermode.sh
+++ configs/changepowermode.sh
@@ -21,37 +21,32 @@ dev_governor=`ls /sys/class/devfreq/qcom,cpubw*/governor`
 case "$powermode" in
     "2")
         #Performance
-        stop mpdecision
         echo 1            > /sys/devices/system/cpu/cpu0/online
         echo 1            > /sys/devices/system/cpu/cpu1/online
         echo 1            > /sys/devices/system/cpu/cpu2/online
         echo 1            > /sys/devices/system/cpu/cpu3/online
-        echo 0            > /sys/devices/system/cpu/sched_mc_power_savings
-        echo performance  > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
-        echo performance  > /sys/devices/system/cpu/cpu1/cpufreq/scaling_governor
-        echo performance  > /sys/devices/system/cpu/cpu2/cpufreq/scaling_governor
-        echo performance   > /sys/devices/system/cpu/cpu3/cpufreq/scaling_governor
+        echo 2            > /sys/devices/system/cpu/sched_mc_power_savings
+        echo interactive  > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
+        echo interactive  > /sys/devices/system/cpu/cpu1/cpufreq/scaling_governor
+        echo interactive  > /sys/devices/system/cpu/cpu2/cpufreq/scaling_governor
+        echo interactive  > /sys/devices/system/cpu/cpu3/cpufreq/scaling_governor
+        echo "10000 1400000:20000 1700000:10000" > /sys/devices/system/cpu/cpufreq/interactive/above_hispeed_delay
+        echo 98            > /sys/devices/system/cpu/cpufreq/interactive/go_hispeed_load
+        echo 1497600       > /sys/devices/system/cpu/cpufreq/interactive/hispeed_freq
+        echo 40000         > /sys/devices/system/cpu/cpufreq/interactive/min_sample_time
         echo 20            > /sys/module/cpu_boost/parameters/boost_ms
+        echo 1728000       > /sys/module/cpu_boost/parameters/sync_threshold
         echo 1497600       > /sys/module/cpu_boost/parameters/input_boost_freq
         echo 40            > /sys/module/cpu_boost/parameters/input_boost_ms
-        echo 1728000       > /sys/module/cpu_boost/parameters/sync_threshold
-        echo 578000000     > /sys/class/kgsl/kgsl-3d0/max_gpuclk
-        echo performance   > /sys/class/kgsl/kgsl-3d0/devfreq/governor
-        echo "msm_cpufreq" > $dev_governor
-        case "$target" in
-            "MI 3W")
-                echo 2265600 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
-                echo 2265600 > /sys/devices/system/cpu/cpu1/cpufreq/scaling_max_freq
-                echo 2265600 > /sys/devices/system/cpu/cpu2/cpufreq/scaling_max_freq
-                echo 2265600 > /sys/devices/system/cpu/cpu3/cpufreq/scaling_max_freq
-                ;;
-            "MI 4"*)
-                echo 2457600 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
-                echo 2457600 > /sys/devices/system/cpu/cpu1/cpufreq/scaling_max_freq
-                echo 2457600 > /sys/devices/system/cpu/cpu2/cpufreq/scaling_max_freq
-                echo 2457600 > /sys/devices/system/cpu/cpu3/cpufreq/scaling_max_freq
-                ;;
-        esac
+        echo 330000000     > /sys/class/kgsl/kgsl-3d0/max_gpuclk
+        echo msm-adreno-tz > /sys/class/kgsl/kgsl-3d0/devfreq/governor
+        echo "cpubw_hwmon" > $dev_governor
+        echo 1958400 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
+        echo 1958400 > /sys/devices/system/cpu/cpu1/cpufreq/scaling_max_freq
+        echo 1958400 > /sys/devices/system/cpu/cpu2/cpufreq/scaling_max_freq
+        echo 1958400 > /sys/devices/system/cpu/cpu3/cpufreq/scaling_max_freq
+        echo "85 1500000:95" > /sys/devices/system/cpu/cpufreq/interactive/target_loads
+        start mpdecision
         ;;
     "1")
         #Balanced
@@ -72,25 +67,14 @@ case "$powermode" in
         echo 1728000                             > /sys/module/cpu_boost/parameters/sync_threshold
         echo 1497600                             > /sys/module/cpu_boost/parameters/input_boost_freq
         echo 40                                  > /sys/module/cpu_boost/parameters/input_boost_ms
-        echo 578000000                           > /sys/class/kgsl/kgsl-3d0/max_gpuclk
-        echo msm-adreno-tz                       > /sys/class/kgsl/kgsl-3d0/devfreq/governor
+        echo 200000000                           > /sys/class/kgsl/kgsl-3d0/max_gpuclk
+        echo powersave                           > /sys/class/kgsl/kgsl-3d0/devfreq/governor
         echo "cpubw_hwmon"                       > $dev_governor
-        case "$target" in
-            "MI 3W")
-                echo 2265600                    > /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
-                echo 2265600                    > /sys/devices/system/cpu/cpu1/cpufreq/scaling_max_freq
-                echo 2265600                    > /sys/devices/system/cpu/cpu2/cpufreq/scaling_max_freq
-                echo 2265600                    > /sys/devices/system/cpu/cpu3/cpufreq/scaling_max_freq
-                echo "85 1500000:90 1800000:70" > /sys/devices/system/cpu/cpufreq/interactive/target_loads
-                ;;
-            "MI 4"*)
-                echo 2457600         > /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
-                echo 2457600         > /sys/devices/system/cpu/cpu1/cpufreq/scaling_max_freq
-                echo 2457600         > /sys/devices/system/cpu/cpu2/cpufreq/scaling_max_freq
-                echo 2457600         > /sys/devices/system/cpu/cpu3/cpufreq/scaling_max_freq
-                echo "85 1500000:99" > /sys/devices/system/cpu/cpufreq/interactive/target_loads
-                ;;
-        esac
+        echo 1958400 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
+        echo 1958400 > /sys/devices/system/cpu/cpu1/cpufreq/scaling_max_freq
+        echo 1958400 > /sys/devices/system/cpu/cpu2/cpufreq/scaling_max_freq
+        echo 1958400 > /sys/devices/system/cpu/cpu3/cpufreq/scaling_max_freq
+        echo "90 1500000:99" > /sys/devices/system/cpu/cpufreq/interactive/target_loads
         start mpdecision
         ;;
     "0")
@@ -101,17 +85,17 @@ case "$powermode" in
         echo 0             > /sys/devices/system/cpu/cpu2/online
         echo 0             > /sys/devices/system/cpu/cpu3/online
         echo 2             > /sys/devices/system/cpu/sched_mc_power_savings
-        echo conservative  > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
-        echo conservative  > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
-        echo conservative  > /sys/devices/system/cpu/cpu1/cpufreq/scaling_governor
-        echo conservative  > /sys/devices/system/cpu/cpu2/cpufreq/scaling_governor
-        echo conservative  > /sys/devices/system/cpu/cpu3/cpufreq/scaling_governor
+        echo powersave  > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
+        echo powersave  > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
+        echo powersave  > /sys/devices/system/cpu/cpu1/cpufreq/scaling_governor
+        echo powersave  > /sys/devices/system/cpu/cpu2/cpufreq/scaling_governor
+        echo powersave  > /sys/devices/system/cpu/cpu3/cpufreq/scaling_governor
         echo 0             > /sys/module/cpu_boost/parameters/boost_ms
         echo 960000        > /sys/module/cpu_boost/parameters/sync_threshold
         echo 960000        > /sys/module/cpu_boost/parameters/input_boost_freq
         echo 40            > /sys/module/cpu_boost/parameters/input_boost_ms
-        echo 330000000     > /sys/class/kgsl/kgsl-3d0/max_gpuclk
-        echo msm-adreno-tz > /sys/class/kgsl/kgsl-3d0/devfreq/governor
+        echo 200000000     > /sys/class/kgsl/kgsl-3d0/max_gpuclk
+        echo powersave     > /sys/class/kgsl/kgsl-3d0/devfreq/governor
         echo "cpubw_hwmon" > $dev_governor
         echo 1036800       > /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
         echo 1036800       > /sys/devices/system/cpu/cpu1/cpufreq/scaling_max_freq
